ebb77026f5b2953c8417e8786ee80b7e84b64944
Matheus Santos <somathsant@gmail.com>
2022-03-08 18:21:39 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,8 @@
+
+function Form() {
+    return (
+        <h1>Pagina de Formulario</h1>
+    );
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,8 @@
+
+function Listing() {
+    return (
+        <h1>Pagina de Listagem</h1>
+    );
+}
+
+export default Listing;
